12c91152f4e78382eeb6c371aa9b6c9cfa99a282
Aria Moraine <ariamoraine@gmail.com>
2012-07-01 23:52:23 -0400

    adding users

--- ariablog/views.py
+++ ariablog/views.py
@@ -56,8 +56,8 @@ def newuser(request):
 	user = User.objects.create_user(request.POST['username'], request.POST['email'], request.POST['password'])
 	user.first_name=request.POST['firstname']
 	user.last_name=request.POST['lastname']
-	user.is_staff=True
-	user.is_superuser=True
+	user.is_staff=False
+	user.is_superuser=False
 	user.save()
 	html = t.render(Context({'user': user}))
 	return HttpResponse(html)
